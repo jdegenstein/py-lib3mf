afd510cc22a9af89cb04f0684b1147a6f4fef7ab
jdegenstein <jdegenstein@users.noreply.github.com>
2024-05-08 11:42:34 -0500

    __init__.py -> Detect unsupported system and machine type (architecture)

--- py_lib3mf/__init__.py
+++ py_lib3mf/__init__.py
@@ -1,3 +1,18 @@
 __version__ = '2.3.1'
 
+import platform
 from . import Lib3MF
+
+system = platform.system().lower()
+
+if system not in ("linux", "darwin", "windows"):
+    raise OSError(f"Unsupported Operating System: {system}")
+
+machine = platform.machine().lower()
+
+if system == "linux" and machine != "x86_64":
+    raise OSError(f"Unsupported Machine Type: {machine}")
+elif system == "windows" and machine not in ("x86_64", "amd64")
+    raise OSError(f"Unsupported Machine Type: {machine}")
+
+# skipping darwin machine check for now
